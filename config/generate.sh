65c975d8309472bdc775daad0f02c314e78046aa
dumpstertree <dumpstertree@gmail.com>
2024-12-03 16:20:01 -0500

    more

--- config/generate.sh
+++ config/generate.sh
@@ -2,7 +2,7 @@ echo Beginning Scan...
 python /Deej-AI-Auto/MP3ToVec.py data mp3tovec --scan /Deej-AI-Auto/library
 echo Scan Complete!
 echo Beginning Playlist Generation...
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test0.mp3
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test1.mp3
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test2.mp3
+python Deej-A.I.py data mp3tovec --playlist library/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong library/test0.mp3
+python Deej-A.I.py data mp3tovec --playlist library/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong library/test1.mp3
+python Deej-A.I.py data mp3tovec --playlist library/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong library/test2.mp3
 echo Playlist Generation Complete!
