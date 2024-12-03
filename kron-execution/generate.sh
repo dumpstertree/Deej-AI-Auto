73b20c1e08ebae90f4d337affa924dd0166311f8
dumpstertree <dumpstertree@gmail.com>
2024-12-02 20:16:06 -0500

    added echos

--- kron-execution/generate.sh
+++ kron-execution/generate.sh
@@ -1,4 +1,8 @@
+echo Beginning Scan...
 python /Deej-AI-Auto/MP3toVec.py data mp3tovec --scan /Deej-AI-Auto/library
+echo Scan Complete!
+echo Beginning Playlist Generation...
 python /Deej-AI-Auto/Deej-A.I.py data mp3tovec --playlist /Deep-AI-Auto/output/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test0.mp3
 python /Deej-AI-Auto/Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test1.mp3
 python /Deej-AI-Auto/Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong /Deej-AI-Auto/library/test2.mp3
+echo Playlist Generation Complete!
