2827bdab60ab1d60bbbcff952b13aed40903b731
dumpstertree <dumpstertree@gmail.com>
2024-12-02 19:52:00 -0500

    added test file to be executed during a kron job

---
+++ kron-execution/generate.sh
@@ -0,0 +1,4 @@
+python MP3toVec.py data mp3tovec --scan /Deej-AI/library
+python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test0.mp3
+python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test1.mp3
+python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test2.mp3
