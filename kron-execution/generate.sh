f1968257d7bb0333716503f8af26bde981aef10d
dumpstertree <dumpstertree@gmail.com>
2024-12-02 19:57:16 -0500

    fixed type

--- kron-execution/generate.sh
+++ kron-execution/generate.sh
@@ -1,4 +1,4 @@
-python MP3toVec.py data mp3tovec --scan /Deej-AI/library
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test0.mp3
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test1.mp3
-python Deej-A.I.py data mp3tovec --playlist /Deej-AI/output/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI/library/test2.mp3
+python MP3toVec.py data mp3tovec --scan /Deej-AI-Auto/library
+python Deej-A.I.py data mp3tovec --playlist /Deep-AI-Auto/output/playlist_a.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI-Auto/library/test0.mp3
+python Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_b.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI-Auto/library/test1.mp3
+python Deej-A.I.py data mp3tovec --playlist /Deej-AI-Auto/output/playlist_c.m3u --nsongs 10 --lookback 10 --inputsong Deej-AI-Auto/library/test2.mp3
