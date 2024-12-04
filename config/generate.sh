0f2e9009d6029b07c2557aa6a0215dd0957ab49a
dumpstertree <dumpstertree@gmail.com>
2024-12-04 09:35:22 -0500

    update generate.sh

--- config/generate.sh
+++ config/generate.sh
@@ -2,7 +2,8 @@ echo Beginning Scan...
 python /Deej-AI-Auto/MP3ToVec.py data mp3tovec --scan /library
 echo Scan Complete!
 echo Beginning Playlist Generation...
-python Deej-A.I.py data mp3tovec --playlist /library/playlist_a.m3u --nsongs 99 --lookback 10 --inputsong /library/test0.mp3
-python Deej-A.I.py data mp3tovec --playlist /library/playlist_b.m3u --nsongs 99 --lookback 10 --inputsong /library/test1.mp3
-python Deej-A.I.py data mp3tovec --playlist /library/playlist_c.m3u --nsongs 99 --lookback 10 --inputsong /library/test2.mp3
+python Deej-A.I.py data mp3tovec --playlist /library/playlist_a.m3u --nsongs 99 --lookback 50 --inputsong /library/test0.mp3
+python Deej-A.I.py data mp3tovec --playlist /library/playlist_b.m3u --nsongs 99 --lookback 50 --inputsong /library/test1.mp3
+python Deej-A.I.py data mp3tovec --playlist /library/playlist_c.m3u --nsongs 99 --lookback 50 --inputsong /library/test2.mp3
+python Deej-A.I.py data mp3tovec --playlist /library/playlist_c.m3u --nsongs 99 --lookback 50 --inputsong /library/test0.mp3 /library/test1.mp3 /library/test2.mp3
 echo Playlist Generation Complete!
